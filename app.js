8dc64a437438b1fa17f306e6607176a3d40fc8c5
Isac X <isacssw@gmail.com>
2021-03-05 12:57:45 +0000

    blog update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require("lodash");
 let posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -42,7 +43,17 @@ app.post("/compose", function(req, res){
   res.redirect("/");
 })
 
+app.get("/posts/:postName", function(req, res){
+  let requestedTitle = _.lowerCase(req.params.postName);
 
+  posts.forEach(function(post) {
+    if (requestedTitle === _.lowerCase(post.title)) {
+      res.render("post", {selectedPost: post});
+    } else {
+      console.log("not match");
+    }
+  });
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
